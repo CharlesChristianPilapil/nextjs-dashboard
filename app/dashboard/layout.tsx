a858c059809af6f72685e3c07ceabb2df2775423
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2024-06-22 16:28:41 +0800

    commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { Chocolate_Classical_Sans } from 'next/font/google';
+import SideNav from '../ui/dashboard/sidenav';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-ful flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
